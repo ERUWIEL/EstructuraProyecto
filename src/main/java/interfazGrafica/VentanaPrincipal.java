a7603608711b50cd68df406b1bc55d84e6a8829d
erwbyel <138058569+ERUWIEL@users.noreply.github.com>
2025-05-09 11:27:24 -0600

    init commit

---
+++ src/main/java/interfazGrafica/VentanaPrincipal.java
@@ -0,0 +1,17 @@
+
+package interfazGrafica;
+
+import javax.swing.JFrame;
+
+/**
+ * clase que inicia los metodos visuales
+ * @author erwbyel
+ */
+public class VentanaPrincipal extends JFrame {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("HOLA MUNDO");
+    }
+}
